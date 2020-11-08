d3c7ab9d6cf55272463c81f69415c661ee4d6e4c
Miguel Angel Pantoja <mgbastechnology@gmail.com>
2020-11-08 13:48:05 +0100

    realizado rename de la app con todas sus actualizaciones

--- innosoft_api/innosoft_api/settings.py
+++ innosoft_api/innosoft_api/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'ponentes',
+    'programa',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- innosoft_api/innosoft_api/urls.py
+++ innosoft_api/innosoft_api/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ponentes/', include('ponentes.urls'))
+    path('ponentes/', include('programa.urls'))
 ]
