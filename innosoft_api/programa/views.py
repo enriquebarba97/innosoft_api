5925da3a9ad5c132cfedce8cc58797c15284d388
Miguel Angel Pantoja <mgbastechnology@gmail.com>
2020-12-04 14:03:57 +0100

    feat (config): Nuevos permisos y grupo
    Restricciones de acceso a la API con permissions

--- innosoft_api/programa/views.py
+++ innosoft_api/programa/views.py
@@ -3,22 +3,45 @@ from rest_framework import generics
 # Create your views here.
 from .models import *
 from .serializers import *
+from .permissions import *
 
 class PonenteView(generics.ListCreateAPIView):
+  permission_classes = [HasGroupPermission]
+  required_groups = {
+         'GET': ['__all__'],
+     }
   queryset = Ponente.objects.all()
   serializer_class = PonenteSerializer
 
 class PonenteRetrieveUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+  permission_classes = [HasGroupPermission]
+  required_groups = {
+         'GET': ['__all__'],
+         'POST': ['administrador'],
+         'PUT': ['administrador','moderador'],
+         'DELETE': ['administrador'],
+     }
   #lookup_field = 'id'
   queryset = Ponente.objects.all()
   serializer_class = PonenteSerializer
   
 
 class PonenciaView(generics.ListCreateAPIView):
+  permission_classes = [HasGroupPermission]
+  required_groups = {
+         'GET': ['__all__'],
+     }
   queryset = Ponencia.objects.all()
   serializer_class = PonenciaSerializer
 
 class PonenciaRetrieveUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+  permission_classes = [HasGroupPermission]
+  required_groups = {
+         'GET': ['__all__'],
+         'POST': ['administrador'],
+         'PUT': ['administrador','moderador'],
+         'DELETE': ['administrador'],
+     }
   #lookup_field = 'id'
   queryset = Ponencia.objects.all()
   serializer_class = PonenciaSerializer
