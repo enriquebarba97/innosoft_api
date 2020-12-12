2e56866db927b9795b1866b416e9efd461b6bde7
MoisesPL <pantion.loza.moises@gmail.com>
2020-12-12 16:34:46 +0100

    fix:  Asistencia url arregladas

--- innosoft_api/participacion/urls.py
+++ innosoft_api/participacion/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from participacion.views import Asistencia
+from .views import *
 
 urlpatterns = [
-    path("/asistencias", AsistenciaView.as_view(), name="asistencias_view"),
-    path("/asistencias/<int:pk>", AsistenciaRetrieveUpdateDelete.as_view(),name='asistencia_retrieve_update_delete')
+    path("asistencias/", AsistenciaView.as_view(), name="asistencias_view"),
+    path("asistencias/<int:pk>", AsistenciaRetrieveUpdateDelete.as_view(),name='asistencia_retrieve_update_delete')
 
 
 ]
