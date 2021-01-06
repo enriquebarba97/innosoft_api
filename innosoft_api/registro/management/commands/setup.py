4e1bf5c1f10dfdc18ae80544ce0b46e6299edffa
Moipanloz <pantion.loza.moises@gmail.com>
2021-01-06 18:27:44 +0100

    fix: TravisCI
    -Cambiado setup.py a comando
    -DJango hace tests y codacy coverage
    -TravisCI hace tests

--- innosoft_api/registro/management/commands/setup.py
+++ innosoft_api/registro/management/commands/setup.py
@@ -1,15 +1,18 @@
 import os
+from django.core.management.base import BaseCommand
 
 #Sets up the system
 
-os.system("python .\innosoft_api\manage.py makemigrations registro")
-os.system("python .\innosoft_api\manage.py migrate registro")
-os.system("python .\innosoft_api\manage.py group")
-os.system("python .\innosoft_api\manage.py migrate")
-os.system("python .\innosoft_api\manage.py makemigrations programa")
-os.system("python .\innosoft_api\manage.py migrate programa")
-os.system("python .\innosoft_api\manage.py makemigrations participacion")
-os.system("python .\innosoft_api\manage.py migrate participacion")
-os.system("python .\innosoft_api\manage.py createAdmin")
-os.system("python .\innosoft_api\manage.py runserver")
-        
+class Command(BaseCommand):
+    help = 'Sets up the system'
+
+    def handle(self, *args, **kwargs):
+        os.system("python .\innosoft_api\manage.py makemigrations registro")
+        os.system("python .\innosoft_api\manage.py migrate registro")
+        os.system("python .\innosoft_api\manage.py group")
+        os.system("python .\innosoft_api\manage.py migrate")
+        os.system("python .\innosoft_api\manage.py makemigrations programa")
+        os.system("python .\innosoft_api\manage.py migrate programa")
+        os.system("python .\innosoft_api\manage.py makemigrations participacion")
+        os.system("python .\innosoft_api\manage.py migrate participacion")
+        os.system("python .\innosoft_api\manage.py createAdmin")
