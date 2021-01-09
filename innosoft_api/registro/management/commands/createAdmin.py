660e35ce55af7defdb4fbe7a96cc8f71c6517469
Carlos Cote Medina <carcotmed@gmail.com>
2021-01-09 17:26:43 +0100

    fix:added the error import

--- innosoft_api/registro/management/commands/createAdmin.py
+++ innosoft_api/registro/management/commands/createAdmin.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from registro.models import User
 from django.contrib.auth.models import Group
+from django.db.utils import IntegrityError
 
 
 class Command(BaseCommand):
