3e29d2dd993764cc6491750f0d990ba76e934102
Carlos Cote Medina <carcotmed@gmail.com>
2021-01-16 21:03:46 +0100

    dep:added ngix files

--- docker/docker-settings.py
+++ docker/docker-settings.py
@@ -1,13 +1,3 @@
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'HOST': 'db',
-#         'PORT': 5432,
-#     }
-# }
-
 BASEURL = 'http://10.5.0.1:8000'
 
 STATIC_ROOT = '/app/static/'
